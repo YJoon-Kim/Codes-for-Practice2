c8e6bc263804fb9af2d2e73a1a32c45bb6d738dc
YJoon-Kim <kimyj25351@gmail.com>
2022-01-01 22:20:37 +0900

    .
    .

--- Project1_newstart/gooood.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main2()
-{
-	printf("Hello, World!\n");
-	printf("Hello, World!\n");
-
-	return 0;
-}
--- Project1_newstart/great.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-void main3()
-{
-	printf("Hello, World!\n");
-}
