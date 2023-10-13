08a9559e03b5d40b58e5754711b3fe1312e6d8b1
YJoon-Kim <89899265+YJoon-Kim@users.noreply.github.com>
2023-10-13 08:59:09 +0000

    first commit

--- Code moeum/C Language/TBC_Codes/helloworld.c
+++ Code moeum/C Language/TBC_Codes/helloworld.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
+void say_hello(void);
+
 int main()
 {
-    printf("\n");
-    
-    printf("Hello World!\n\n\n");
+    // print formatted -> printf
+
+    int x, y, z;
+
+    x = 1;
+    y = 2;
+    z = 4;
+
+    z = x + y;
+
+    say_hello();
 
     return 0;
+}
+
+void say_hello(void)
+{
+    int x = 1;
+    x = 10;
+
+    printf("Hello World!\n");
+
+    return;
 }
